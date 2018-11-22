f453ff81b63cc2bc767a319dd6581e12f29b39e0
Pedro Castro <p.castro275@gmail.com>
2018-11-22 17:36:31 +0000

    Install Angular Material and FxFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,13 +16,15 @@ import { CreateItemComponent } from './dashboard/create-item/create-item.compone
     DashboardComponent,
     ItemsListComponent,
     ItemDetailComponent,
-    CreateItemComponent,
+    CreateItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
