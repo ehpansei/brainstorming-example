63bab7824572c8716d3a743ba9cf589f663a9557
Pedro Castro <p.castro275@gmail.com>
2018-11-22 16:14:00 +0000

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ListComponent } from './parent/list/list.component';
+import { DetailComponent } from './parent/detail/detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    ListComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
